133822a1cb7803eb2c9cf5ea092d902750c5703a
Mariusz Lotko <mariusz.lotko@gmail.com>
2017-04-09 22:47:54 +0200

    Test cleanup

---
+++ src/test/groovy/pl/lotko/type/tuple/TupleFactoryTest.groovy
@@ -0,0 +1,31 @@
+package pl.lotko.type.tuple
+
+import spock.lang.Specification
+
+import static pl.lotko.type.tuple.TupleFactory.of;
+
+class TupleFactoryTest extends Specification {
+    def "two arguments create a Pair"() {
+        when:
+        def pair = of("String", 123)
+
+        then:
+        pair instanceof Pair
+        !(pair instanceof Triple)
+        pair.first == "String"
+        pair.second == 123
+    }
+
+    def "three arguments create a Triple"() {
+        when:
+        def triple = of("String", 123, 1.0)
+
+        then:
+        triple instanceof Triple
+        !(triple instanceof Quadruple)
+        triple.first == "String"
+        triple.second == 123
+        triple.third == new BigDecimal("1.0")
+    }
+
+}
